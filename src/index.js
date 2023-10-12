07475583d723b207bd368e6f52f554a8b6d7fddd
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-10-11 19:08:27 -0500

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const form = document.getElementById("create-task-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+const taskInput = document.getElementById("new-task-description");
+const taskDescription = taskInput.value;
+
+if (taskDescription) {
+  const taskList = document.getElementById("tasks");
+  const newTask = document.createElement("li");
+  newTask.textContent = taskDescription;
+  taskList.appendChild(newTask);
+  taskInput.value = "";
+}
+if (taskDescription) {
+  const newTask = document.createElement("li");
+  newTask.innerHTML = `
+    ${taskDescription}
+    <button class="delete-button">Delete</button>
+  `;
+  const deleteButton = newTask.querySelector(".delete-button");
+  deleteButton.addEventListener("click", function () {
+    taskList.removeChild(newTask);
+  });
+
+  taskList.appendChild(newTask);
+  taskInput.value = "";
+}
